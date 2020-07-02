85aeea64fc339baa0691a15ab3693349b7e97428
soumend80 <37365771+soumend80@users.noreply.github.com>
2020-07-02 05:32:58 +0530

    Update SavefilefromInternet.m

--- Miscellaneous/SavefilefromInternet.m
+++ Miscellaneous/SavefilefromInternet.m
@@ -1,8 +1,11 @@
-clearvars
-targetfolder='E:\Lecture Notes\Brownian Motion';
+targetfolder='D:\Money Heist\S03';
 mkdir(targetfolder)
-for i=1:10
-	url = strcat('http://physics.gu.se/~frtbm/joomla/media/mydocs/LennartSjogren/kap',num2str(i),'.pdf');
-	filename = strcat(targetfolder,'/Chapter ',num2str(i),'.pdf');
-	outfilename = websave(filename,url);
+t=tic;
+parfor i=1:4
+    tic
+    url = strcat('http://dl2.mojdl.com/upload/Tv-Series/Money%20Heist/S03/1080p/Money_Heist_S03E',num2str(i,'%02d'),'_x265_1080p_WEBRip_[Mojoo].mkv');
+    filename = strcat(targetfolder,'\Money_Heist_S03E',num2str(i,'%02d'),'_x265_1080p_WEBRip_[Mojoo].mkv');
+    outfilename = websave(filename,url);
+    toc
 end
+toc(t)
